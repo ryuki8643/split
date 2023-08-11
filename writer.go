9dbf7e38cd9af133e74a117e8b3ae67553028df0
ryuki8643 <garyuki88me@gmail.com>
2023-08-11 23:57:22 +0900

    write logic for piecelinesplitter

--- writer.go
+++ writer.go
@@ -277,3 +277,119 @@ func (s PieceSplitter) Split(file *os.File, fileNameCreater FileNameCreater) err
 	fmt.Println("File splitting is complete.")
 	return nil
 }
+
+type PieceLineSplitter struct {
+	separatePieceNumberStr int64
+}
+
+func (s PieceLineSplitter) Split(file *os.File, fileNameCreater FileNameCreater) error {
+	
+    // Set the maximum memory limit to 1GB (in bytes).
+    const maxMemoryLimit = 1 * 1024 * 1024 * 1024
+
+    // Initialize a buffer to store file data temporarily.
+    buffer := make([]byte, 0)
+
+	// count file line number
+	fileLineNum := countLinesByFile(file)
+
+
+	fileSizePerPiece := fileLineNum / s.separatePieceNumberStr
+
+	if fileLineNum%s.separatePieceNumberStr != 0 {
+		fileSizePerPiece++
+	}
+
+    // Line counter to keep track of lines read from the input file.
+    var lineCounter int64 = 0
+
+    // Output file counter to keep track of split files.
+    outputCounter := 0
+
+    // Read the input file line by line.
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := scanner.Text()
+
+        // Increase the line counter.
+        lineCounter++
+
+        // Add the line to the buffer.
+        buffer = append(buffer, line...)
+        buffer = append(buffer, '\n') // Add a newline character after each line.
+
+        // If we have read 1000 lines, write to the output file.
+        if lineCounter == fileSizePerPiece {
+            // Create the output file.
+            outputFilePath,err := fileNameCreater.Create(outputCounter)
+            if err != nil {
+                return err
+            }
+            outFile, err := os.Create(outputFilePath)
+            if err != nil {
+                return fmt.Errorf(createFileErrorMsg,err)
+            }
+
+            // Write the buffer data to the output file.
+            _, err = outFile.Write(buffer)
+            if err != nil {
+                return fmt.Errorf(fileWriteErrorMsg, err)
+            }
+
+            // Close the output file.
+            outFile.Close()
+
+            // Reset the buffer and line counter for the next output file.
+            buffer = buffer[:0]
+            lineCounter = 0
+
+            // Increment the output file counter.
+            outputCounter++
+
+        }
+
+        if len(buffer) > maxMemoryLimit {
+            return fmt.Errorf(maxMemoryLimitExceededErrorMsg)
+        }
+    }
+
+	if len(buffer) > 0 {
+            // Create the output file.
+            outputFilePath,err := fileNameCreater.Create(outputCounter)
+            if err != nil {
+                return err
+            }
+            outFile, err := os.Create(outputFilePath)
+            if err != nil {
+                return fmt.Errorf(createFileErrorMsg,err)
+            }
+
+            // Write the buffer data to the output file.
+            _, err = outFile.Write(buffer)
+            if err != nil {
+                return fmt.Errorf(fileWriteErrorMsg, err)
+            }
+
+            // Close the output file.
+            outFile.Close()
+
+            // Increment the output file counter.
+            outputCounter++
+	}
+
+    if err := scanner.Err(); err != nil {
+        return fmt.Errorf(fileReadErrorMsg, err)
+    }
+
+    fmt.Println("File splitting is complete.")
+    return nil
+}
+
+func countLinesByFile(file *os.File) int64 {
+	var count int64
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		count++
+	}
+	return count
+}
