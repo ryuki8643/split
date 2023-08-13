880b53dc34f486b370607df8d3fb44e46fde5732
ryuki8643 <garyuki88me@gmail.com>
2023-08-13 18:41:01 +0900

    modify read and write line one by one in piecelinesplitter

--- writer.go
+++ writer.go
@@ -58,7 +58,7 @@ func (s LineSplitter) Split(file *os.File, fileNameCreater FileNameCreater) erro
 		lineCounter++
 
 		// Add the line to the buffer.
-		err = writeFileBy1Line(outFile, line, buffer)
+		buffer,err = writeFileBy1Line(outFile, line, buffer)
 		if err != nil {
 			return err
 		}
@@ -72,19 +72,19 @@ func (s LineSplitter) Split(file *os.File, fileNameCreater FileNameCreater) erro
 	return nil
 }
 
-func writeFileBy1Line(outFile *os.File, line string, buffer []byte) error {
+func writeFileBy1Line(outFile *os.File, line string, buffer []byte) ([]byte,error) {
 	buffer = append(buffer, line...)
 	buffer = append(buffer, '\n') // Add a newline character after each line.
 
 	// Write the buffer data to the output file.
 	_, err := outFile.Write(buffer)
 	if err != nil {
-		return fmt.Errorf(fileWriteErrorMsg, err)
+		return nil,fmt.Errorf(fileWriteErrorMsg, err)
 	}
 
 	// Reset the buffer and line counter for the next output file.
 	buffer = buffer[:0]
-	return nil
+	return buffer,nil
 }
 
 type ByteSplitter struct {
@@ -295,11 +295,11 @@ type PieceLineSplitter struct {
 
 func (s PieceLineSplitter) Split(file *os.File, fileNameCreater FileNameCreater) error {
 
-	// Set the maximum memory limit to 1GB (in bytes).
-	const maxMemoryLimit = 1 * 1024 * 1024 * 1024
+
 
 	// Initialize a buffer to store file data temporarily.
 	buffer := make([]byte, 0)
+	var outFile *os.File
 
 	// count file line number
 	fileLineNum, err := countLinesByFile(file)
@@ -324,46 +324,37 @@ func (s PieceLineSplitter) Split(file *os.File, fileNameCreater FileNameCreater)
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Increase the line counter.
-		lineCounter++
-
-		// Add the line to the buffer.
-		buffer = append(buffer, line...)
-		buffer = append(buffer, '\n') // Add a newline character after each line.
-
 		// If we have read 1000 lines, write to the output file.
-		if lineCounter == fileLinesPerPiece {
+		if lineCounter==0 || lineCounter % fileLinesPerPiece == 0 {
 			// Create the output file.
 			outputFilePath, err := fileNameCreater.Create(outputCounter)
 			if err != nil {
 				return err
 			}
-			outFile, err := os.Create(outputFilePath)
+			outFile, err = os.Create(outputFilePath)
 			if err != nil {
 				return fmt.Errorf(createFileErrorMsg, err)
 			}
 
-			// Write the buffer data to the output file.
-			_, err = outFile.Write(buffer)
-			if err != nil {
-				return fmt.Errorf(fileWriteErrorMsg, err)
-			}
 
 			// Close the output file.
-			outFile.Close()
+			defer outFile.Close()
 
-			// Reset the buffer and line counter for the next output file.
-			buffer = buffer[:0]
-			lineCounter = 0
 
 			// Increment the output file counter.
 			outputCounter++
 
 		}
 
-		if len(buffer) > maxMemoryLimit {
-			return fmt.Errorf(maxMemoryLimitExceededErrorMsg)
+		// Increase the line counter.
+		lineCounter++
+
+		buffer,err = writeFileBy1Line(outFile, line,buffer)
+		if err != nil {
+			return err
 		}
+
+		
 	}
 
 	if len(buffer) > 0 {
@@ -441,7 +432,7 @@ func (s PieceLineRoundRobinSplitter) Split(file *os.File, fileNameCreater FileNa
 		// Create the output file.
 		outFile := outFiles[lineCounter%int(s.separatePieceNumber)]
 
-		err = writeFileBy1Line(outFile, line, buffer)
+		buffer,err = writeFileBy1Line(outFile, line, buffer)
 		if err != nil {
 			return err
 		}
